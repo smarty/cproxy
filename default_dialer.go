524865ea2ebe47988a2f4b1e55b6894740e883bf
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2017-08-25 09:17:12 -0600

    Added logging when dialer is unable to establish a connection.

--- default_dialer.go
+++ default_dialer.go
@@ -3,10 +3,14 @@ package cproxy
 import (
 	"net"
 	"time"
+
+	"github.com/smartystreets/logging"
 )
 
 type DefaultDialer struct {
 	timeout time.Duration
+	logging bool
+	logger  *logging.Logger
 }
 
 func NewDialer() *DefaultDialer {
@@ -17,10 +21,17 @@ func NewDialerWithTimeout(timeout time.Duration) *DefaultDialer {
 	return &DefaultDialer{timeout: timeout}
 }
 
+func (this *DefaultDialer) WithLogging() *DefaultDialer {
+	this.logging = true
+	return this
+}
+
 func (this *DefaultDialer) Dial(address string) Socket {
-	if socket, err := net.DialTimeout("tcp", address, this.timeout); err != nil {
-		return nil
-	} else {
+	if socket, err := net.DialTimeout("tcp", address, this.timeout); err == nil {
 		return socket
+	} else if this.logging {
+		this.logger.Printf("[INFO] Unable to establish connection to [%s]: %s", address, err)
 	}
+
+	return nil
 }
